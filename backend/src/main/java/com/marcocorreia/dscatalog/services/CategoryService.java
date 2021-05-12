d3f38808c6abea96ee09bc8408b2aaa3ae475ff5
Marco Correia <m.correia.m@gmail.com>
2021-05-12 15:37:59 -0300

    category getById

--- backend/src/main/java/com/marcocorreia/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/marcocorreia/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.marcocorreia.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,12 @@ public class CategoryService {
 	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();		
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());				
+	}
+
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 
 }
