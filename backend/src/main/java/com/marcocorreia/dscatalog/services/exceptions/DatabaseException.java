f9294acf06ff7146528b4544ff7346d78059a35e
Marco Correia <m.correia.m@gmail.com>
2021-05-14 09:51:10 -0300

    Auditing data

---
+++ backend/src/main/java/com/marcocorreia/dscatalog/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.marcocorreia.dscatalog.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
