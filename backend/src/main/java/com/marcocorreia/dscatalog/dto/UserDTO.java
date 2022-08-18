9fee369d44d7e704d735ff69c87aa4e15a631a15
mcorreiam <m.correia.m@gmail.com>
2022-08-18 14:39:49 -0300

    validations beans validation

--- backend/src/main/java/com/marcocorreia/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/marcocorreia/dscatalog/dto/UserDTO.java
@@ -4,14 +4,21 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.marcocorreia.dscatalog.entities.User;
 
 public class UserDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long 	id;
+	
+	@NotBlank(message = "Campo requerido")
 	private String 	firstName;
 	private String 	lastName;
+	
+	@Email
 	private String 	email;
 	
 	private Set<RoleDTO> roles = new HashSet<>();
