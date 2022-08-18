0d1666a59e94a2327b6d77e96f384aa864f2db2d
mcorreiam <m.correia.m@gmail.com>
2022-08-18 13:42:33 -0300

    CRUD de usuários PARTE 2

---
+++ backend/src/main/java/com/marcocorreia/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.marcocorreia.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcocorreia.dscatalog.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
