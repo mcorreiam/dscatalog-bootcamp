d10de53bd670e864b0b88508bee82bb5792f92e2
mcorreiam <m.correia.m@gmail.com>
2022-08-19 16:53:23 -0300

    ajuste imports user entity

--- backend/src/main/java/com/marcocorreia/dscatalog/entities/User.java
+++ backend/src/main/java/com/marcocorreia/dscatalog/entities/User.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.persistence.Column;
